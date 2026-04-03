7691b45002d57faababcf9e6158a7019d1221a08
the-feels-twice <unapologetic.apologizes@gmail.com>
2026-04-03 13:34:44 -0400

    Task #4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 class House {
     boolean destroyed = false;
+    long networth = 0L;
 
     public House(boolean isDestroyed) {
+        this(isDestroyed, 0L);
+    }
+
+    public House(boolean isDestroyed, long networth) {
         destroyed = isDestroyed;
+        this.networth = networth;
+    }
+
+    public void destroy() {
+        this.destroyed = true;
     }
 
     public void rebuild() {
@@ -20,6 +30,14 @@ public class Main {
                 new House(true)
         };
 
+        House[] task4 = {
+                new House(false, 8921000000L),
+                new House(false, 7000880000L),
+                new House(false, 9400000000L),
+                new House(false, 8230000000L),
+                new House(false, 11000670000L),
+        };
+
         System.out.print("---------- Task #1 ----------\n");
         System.out.print("Is house one destroyed? " + task1.destroyed + '\n');
 
@@ -27,7 +45,6 @@ public class Main {
         for (House house : task2) {
             System.out.print("Is house destroyed? " + house.destroyed + '\n');
         }
-
 
         System.out.print("---------- Task #3 ----------\n");
 
@@ -37,6 +54,16 @@ public class Main {
 
         for (House house : task2) {
             System.out.print("Is house destroyed? " + house.destroyed + '\n');
+        }
+
+        System.out.print("---------- Task #4 ----------\n");
+
+        for (House house : task4) {
+           if (house.networth < 8640000000L) {
+               house.destroy();
+           }
+
+           System.out.print("Is house worth enough? " + house.destroyed + '\n');
         }
     }
 }
